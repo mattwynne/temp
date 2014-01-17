f7b7391e49139938670f0a0fb737a7d964ac9ee8
Matt Wynne <matt@mattwynne.net>
2014-01-17 15:51:27 +0000

    Add very long step def

--- features/step_definitions/steps.rb
+++ features/step_definitions/steps.rb
@@ -14,6 +14,10 @@ Given(/^I have found the car of my dreams$/) do
   pending # express the regexp above with the code you wish you had
 end
 
+Given('a very long step definition indeed. So long that it goes on and on and right off the edge of the page i mean like this yeah') do
+  pending
+end
+
 When(/^I buy the car$/) do
   pending # express the regexp above with the code you wish you had
 end
